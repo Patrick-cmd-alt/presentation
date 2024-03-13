76290c94b2188c024f1c29be58c6ec14d6d13d37
Patrick-cmd-alt <156078929+Patrick-cmd-alt@users.noreply.github.com>
2024-03-13 21:26:08 +0100

    back up and titanic code deleted

--- Back up/streamlit_app.py
+++ Back up/streamlit_app.py
@@ -78,8 +78,63 @@ if  page == pages[2] :
     st.image("archive/betting.png", caption='Match count', use_column_width=True)
 
 
+    #diplaying a countplot calculated from the data frame
+    fig = plt.figure()
+    sns.countplot(x = 'Survived', data = df)
+    st.pyplot(fig)
+    gig = plt.figure()
+    sns.countplot(x = 'Sex', data = df)
+    plt.title("Distribution of the passengers gender")
+    st.pyplot(fig)
+
+    fig = plt.figure()
+    sns.countplot(x = 'Pclass', data = df)
+    plt.title("Distribution of the passengers class")
+    st.pyplot(fig)
+
+    fig = sns.displot(x = 'Age', data = df)
+    plt.title("Distribution of the passengers age")
+    st.pyplot(fig)
+    fig = plt.figure()
+    sns.countplot(x = 'Survived', hue='Sex', data = df)
+    st.pyplot(fig)
+
+    fig = sns.catplot(x='Pclass', y='Survived', data=df, kind='point')
+    st.pyplot(fig)
+
+    fig = sns.lmplot(x='Age', y='Survived', hue="Pclass", data=df)
+    st.pyplot(fig)
 
 
+if  page == pages[3] : 
+    # preproccesing and modeling of data
+    st.write("### Modelling")
+    df = df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
+    y = df['Survived']
+    X_cat = df[['Pclass', 'Sex',  'Embarked']]
+    X_num = df[['Age', 'Fare', 'SibSp', 'Parch']]
+    for col in X_cat.columns:
+        X_cat[col] = X_cat[col].fillna(X_cat[col].mode()[0])
+    for col in X_num.columns:
+        X_num[col] = X_num[col].fillna(X_num[col].median())
+    X_cat_scaled = pd.get_dummies(X_cat, columns=X_cat.columns)
+    X = pd.concat([X_cat_scaled, X_num], axis = 1)
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    X_train[X_num.columns] = scaler.fit_transform(X_train[X_num.columns])
+    X_test[X_num.columns] = scaler.transform(X_test[X_num.columns])
+
+    def prediction(classifier):
+        if classifier == 'Random Forest':
+            clf = RandomForestClassifier()
+        elif classifier == 'SVC':
+            clf = SVC()
+        elif classifier == 'Logistic Regression':
+            clf = LogisticRegression()
+        clf.fit(X_train, y_train)
+        return clf
 if  page == pages[4]:
     st.write("#Data")
     # modeling patrick
