9e0e7fc670fc45e1f3f4579911ced8ec41f82aa9
Patrick-cmd-alt <156078929+Patrick-cmd-alt@users.noreply.github.com>
2024-03-13 21:14:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("archive/Patrick_first.csv")
 df2 = pd.read_csv("archive/Patrick_second.csv")
-top20_rf = pd.read_csv("archive/top20.csv")
+top20_rf = pd.read_csv("archive/Patrick_top20.csv")
 
 # defines streamlit layout
 st.title("Tennis prediction project : binary classification project")
