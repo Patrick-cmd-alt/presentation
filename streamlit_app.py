0bea668fd83b840be445188c8e120445964af946
Patrick-cmd-alt <156078929+Patrick-cmd-alt@users.noreply.github.com>
2024-03-14 11:44:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,9 +60,9 @@ if  page == pages[1]:
 
     #display the first 10 lines of the dataframe
     st.write("Initial Data Frame")
-    st.dataframe(df.info())
-    st.dataframe(df.describe())
+    
     st.dataframe(df.head(10))
+    st.dataframe(df.info())
 
     st.write("Enginneerd Data Frame")
     st.dataframe(df2.info())
